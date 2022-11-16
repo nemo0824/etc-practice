4c322b49ad1f7fda6732854b24c5d2c0a01d7272
nemo0824 <59534382+nemo0824@users.noreply.github.com>
2022-11-16 15:20:20 +0900

    11/16

--- 04_Array/src/com/kh/practice/run/Run.java
+++ 04_Array/src/com/kh/practice/run/Run.java
@@ -1,6 +1,6 @@
 package com.kh.practice.run;
 
-import com.kh.array.ArrayPractice;
+import com.kh.array.*;
 
 public class Run {
 
@@ -8,10 +8,30 @@ public class Run {
 		
 		ArrayPractice aa = new ArrayPractice();
 		//aa.practice1();
-		aa.practice2();
-	
+		//aa.practice2();
+			
 		
-
+		B_ArrayCopy bac = new B_ArrayCopy();
+//		bac.method1();
+//		bac.method2();
+//		bac.method3();
+//		bac.method4();
+//		bac.method5();
+//		bac.method6();
+//		bac.method7();
+//		bac.method8();
+//
+//		bac.method9();
+		
+		
+		C_DimensionalArray cda =new C_DimensionalArray();
+//		cda.method1();
+//		cda.method2();
+//		cda.method3();
+//		cda.method4();	
+//		cda.method5();
+//		cda.method6();
+		cda.method7();
 	}
 
 }
